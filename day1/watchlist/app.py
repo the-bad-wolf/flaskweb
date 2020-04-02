0b03d4654df7b91c90be496524ba549fb750b44c
puppies <2426662348@qq.com>
2020-04-02 14:22:31 +0800

    静态文件

--- day1/watchlist/app.py
+++ day1/watchlist/app.py
@@ -1,12 +1,18 @@
-from flask import Flask
+from flask import Flask,render_template
 
 
-app = app = Flask(__name__)
+app = Flask(__name__)
 
 
 @app.route("/")
 def haha():
-    return "<h1>hello world</h1>"
+    movies = [
+        {'title':"囧吗",'year':"2020"},
+        {'title':"大赢家",'year':"2019"},
+        {'title':"速度与激情",'year':"2018"},
+        {'title':"叶问",'year':"2017"},
+    ]
+    return render_template('index.html',movies=movies)
 
 
 
