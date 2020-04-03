a73cbccffd57cb542cf7d3e2408f369b27c60f08
puppies <2426662348@qq.com>
2020-04-03 09:45:13 +0800

    数据库初始化完成，数据导入成功

--- day1/watchlist/app.py
+++ day1/watchlist/app.py
@@ -28,13 +28,10 @@ class movie(db.Model):
 
 @app.route("/")
 def haha():
-    movies = [
-        {'title':"囧吗",'year':"2020"},
-        {'title':"大赢家",'year':"2019"},
-        {'title':"速度与激情",'year':"2018"},
-        {'title':"叶问",'year':"2017"},
-    ]
-    return render_template('index.html',movies=movies)
+    
+    use = user.query.all()
+    heiha = movie.query.all()
+    return render_template('index.html',movies=heiha,use=user)
 
 
 @app.cli.command()
@@ -44,6 +41,26 @@ def initdb():
         db.drop_all()
     db.create_all()
     click.echo("初始化数据库完成")
+
+
+#定义指令  然后flask+函数名  运行
+@app.cli.command()
+def forge():
+    movies = [
+        {'title':"囧吗",'year':"2020"},
+        {'title':"大赢家",'year':"2019"},
+        {'title':"速度与激情",'year':"2018"},
+        {'title':"叶问",'year':"2017"},
+    ]
+    for m in movies:
+        hei = movie(title=m['title'],year=m['year'])
+        db.session.add(hei)
+    db.session.commit()
+    click.echo("导入数据完成")
+
+
+
+
 
 
 
