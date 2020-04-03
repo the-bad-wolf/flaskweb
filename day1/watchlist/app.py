69bae23e64cb9071684f8554adc736b4dcd07075
puppies <2426662348@qq.com>
2020-04-03 09:25:26 +0800

    怎删改查

--- day1/watchlist/app.py
+++ day1/watchlist/app.py
@@ -1,7 +1,29 @@
-from flask import Flask,render_template
+import os,sys
 
+from flask import Flask,render_template
+from flask_sqlalchemy import SQLAlchemy
+import click
+
+WIN = sys.platform.startswith('win')
+if WIN:
+    prefix = "sqlite:///"
+else:
+    prefix = "sqlite:////"
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path,"data.db")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class user(db.Model):
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(10))
+
+class movie(db.Model):
+    id = db.Column(db.Integer,primary_key = True)
+    title = db.Column(db.String(60))
+    year = db.Column(db.String(4))
 
 
 @app.route("/")
@@ -15,7 +37,13 @@ def haha():
     return render_template('index.html',movies=movies)
 
 
-
+@app.cli.command()
+@click.option("--drop",is_flag=True,help="先删除在创建")
+def initdb():
+    if drop:
+        db.drop_all()
+    db.create_all()
+    click.echo("初始化数据库完成")
 
 
 
